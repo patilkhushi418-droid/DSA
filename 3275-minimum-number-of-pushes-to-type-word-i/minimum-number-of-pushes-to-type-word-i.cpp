28f1062a39b0edb7eb8595bcacd718d4a28a9a0f
patilkhushi418-droid <patilkhushi418@gmail.com>
2026-07-30 23:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (54.67%) - LeetSync

---
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.length();
+        int res= 0;
+        for(int i =0 ; i<n ; i++)
+        {
+            res = res + (i/8+1);
+
+        }
+        return res ;
+    }
+};
